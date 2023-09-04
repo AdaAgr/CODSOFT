588e9ef5c94b2c2f062824ef786c0c88ac5d3374
AdaAgr <141999104+AdaAgr@users.noreply.github.com>
2023-09-05 02:23:01 +0530

    Add files via upload

---
+++ credit card fraud detection.py
@@ -0,0 +1,34 @@
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+data = pd.read_csv("credit_card_data.csv")
+
+
+X = data.drop("Class", axis=1)  
+y = data["Class"]              
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+
+clf = RandomForestClassifier(n_estimators=100, random_state=42)
+
+
+clf.fit(X_train, y_train)
+
+
+y_pred = clf.predict(X_test)
+
+# Evaluate the model
+print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
+print("\nClassification Report:\n", classification_report(y_test, y_pred))
+print("\nAccuracy Score:", accuracy_score(y_test, y_pred))
